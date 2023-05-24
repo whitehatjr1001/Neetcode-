7131a26ec953ff0bf8274fb97f8d420b26573ecd
Midigudla Rohith sai <81635089+whitehatjr1001@users.noreply.github.com>
2023-05-24 22:52:08 +0530

    Add files via upload

---
+++ Sortedbinarysearch.java
@@ -0,0 +1,17 @@
+public class Sortedbinarysearch {
+    public static void main(String[] args) {
+        
+    }
+    static int pivoit(int[] arr){
+        int start =0;
+        int end = arr.length -1;
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            
+
+
+        }
+        return end;
+    }
+}
+//rotated binary search
